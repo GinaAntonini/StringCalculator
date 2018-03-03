49a6cdf5e082339bd449b87bbac92db805037a2b
Gina <ginamantonini@gmail.com>
2018-03-03 13:34:00 -0600

    first two tests passing

---
+++ StringCalculator.tests/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace StringCalculator.tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Test_Empty_String()
+        {
+            //Arrange
+            string numbers = string.Empty;
+            var additionCalculator = new AdditionCalculator();
+
+            //Act
+            int result = additionCalculator.AddNumbers("");
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+        
+        [Test]
+        public void Test_For_1_Number_Returns_That_Number_Plus_Zero()
+        {
+            //Arrange
+            var additionCalculator = new AdditionCalculator();
+
+            //Act
+            int result = additionCalculator.AddNumbers("1");
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ StringCalculator/AdditionCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class AdditionCalculator
+    {
+        public int AddNumbers(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var integers = ConvertToNumber(numbers);
+            //I want the sum of integers + 0
+            var sum = SumOfNumbers(integers, integers);
+            //I want the sum of 2 integers
+            return sum;
+        }
+
+        public int ConvertToNumber(string number)
+        {
+            return int.Parse(number);
+        }
+
+        public int SumOfNumbers(int a, int b)
+        {
+            int total;
+            total = a + b;
+            return total;
+        }
+    }
+}
